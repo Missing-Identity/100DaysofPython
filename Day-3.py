4e7c360c4432c1f394887bbe0c69ca035776f2b0
Unmilan Mukherjee <52813146+Missing-Identity@users.noreply.github.com>
2022-01-07 12:39:58 +0530

    Day-3 Project
    Treasure Islan

---
+++ Day-3.py
@@ -0,0 +1,80 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+lor = input("You are at a crossroad, where do you want to go? Left or Right?:")
+if lor == "Left" or lor == "left":
+  print('''Took a left. \n
+
+
+                  _
+             .''.' \    _  __
+ ___         './    '. ' `'  `
+    '._______.'       \
+                       '.__________
+                                   '-.____________
+ _________________________________________________'.__________________
+                                      ____________.'
+                         __________.-'
+      _______          .'                  
+ ___.'       '.       /               '-._    
+             .'\    .' ._,.__,        ____\____.o.
+             '..'._/                 '-._______.-'
+                                     .-'_______'-.
+                                         _/    'o'
+                                      .-'
+                                      
+                                      \n''')
+  water = input("You are faced with a huge mass of water, do you swim or wait?:")
+  if water == "wait" or water == "Wait":
+    print('''Decided to wait\n
+            __________
+           |  __  __  |
+           | |  ||  | |
+           | |  ||  | |
+           | |__||__| |
+           |  __  __()|
+           | |  ||  | |
+           | |  ||  | |
+           | |  ||  | |
+           | |  ||  | |
+           | |__||__| |
+           |__________|
+''')
+    door = input("Magically a set of 3 doors appear in front of you, which door do you choose? Red, Yellow or Blue?:")
+    if door == "Red" or door == "red":
+      print("Opened Red Door. Burned by Fire.\n Game Over!")
+    elif door == "Blue" or "blue":
+      print("Opened Blue Door. Eaten by Beasts.\n Game Over!")
+    elif door == "Yellow" or door == "yellow":
+      print("You Win!")
+    else:
+      print("You couldn't decide on a door!\n Game Over!")
+  else:
+    print("You decided to swim and got attacked by trouts.\n Game Over!")
+else:
+  print("You took a right turn and fell into a hole!\n Game Over!")
