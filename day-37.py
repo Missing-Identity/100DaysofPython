5864096ea4e3663c5c381472d8673a5ea79d239f
Unmilan Mukherjee <52813146+Missing-Identity@users.noreply.github.com>
2022-04-02 14:29:44 +0530

    Day-37
    Day-37-Project: Habit Tracker using Pixelo

---
+++ day-37.py
@@ -0,0 +1,56 @@
+import requests
+import datetime as dt
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": "",
+    "username": "",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+# Create user on Pixela
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+# Create Graph
+graph_endpoint = f"{pixela_endpoint}/{user_params['username']}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Code Graph",
+    "unit": "Days",
+    "type": "int",
+    "color": "ajisai",
+}
+
+headers = {
+    "X-USER-TOKEN": user_params['token'],
+}
+
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response)
+
+today = dt.datetime.now()
+
+pixel_config = {
+    "date": today.strftime("%Y%m%d"),  # Formatting date to match API needs
+    "quantity": "1",
+}
+
+# Posting Pixel to our created graph
+response = requests.post(url=f"{graph_endpoint}/{graph_config['id']}", json=pixel_config, headers=headers)
+print(response)
+
+update_config = {
+    "quantity": "2",
+}
+
+# Update Pixel
+response = requests.put(url=f"{graph_endpoint}/{graph_config['id']}/{pixel_config['date']}", json=update_config,
+                        headers=headers)
+
+# Delete Pixel
+delete = requests.delete(url=f"{graph_endpoint}/{graph_config['id']}/{pixel_config['date']}", headers=headers)
+print(response)
+print(delete)
